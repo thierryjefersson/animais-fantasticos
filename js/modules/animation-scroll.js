099dc9143596e47cfa643eb7519095ea3d1282a9
Thierry <thierryjefersson@gmail.com>
2023-06-26 12:39:12 -0300

    animation-scroll transformado em class

--- js/modules/animation-scroll.js
+++ js/modules/animation-scroll.js
@@ -1,19 +1,25 @@
-export default function initAnimationScroll() {
-  const sections = document.querySelectorAll("[data-anime='scroll']");
-  const halfWindow = window.innerHeight * 0.6;
+export default class initAnimationScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindow = window.innerHeight * 0.6;
 
-  function scrollAnimation() {
-    sections.forEach((section) => {
+    this.scrollAnimation = this.scrollAnimation.bind(this);
+  }
+
+  scrollAnimation() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - halfWindow < 0;
-      if (isSectionVisible) section.classList.add("ativo");
-      else if (section.classList.contains("ativo"))
+      const isSectionVisible = sectionTop - this.halfWindow < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
         section.classList.remove("ativo");
+      }
     });
   }
 
-  if (sections.length) {
-    scrollAnimation();
-    window.addEventListener("scroll", scrollAnimation);
+  init() {
+    this.scrollAnimation();
+    window.addEventListener("scroll", this.scrollAnimation);
   }
 }
