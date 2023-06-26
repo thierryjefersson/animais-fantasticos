6d601d5b849948b62a72eed0f1969c86aa6338e6
Thierry <thierryjefersson@gmail.com>
2023-06-26 19:20:49 -0300

    animation-scroll refeita

--- js/modules/animation-scroll.js
+++ js/modules/animation-scroll.js
@@ -3,25 +3,45 @@ export default class AnimationScroll {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
 
-    this.scrollAnimation = this.scrollAnimation.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  scrollAnimation() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfWindow < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // Armazena a distancia de cada item em relacao ao
+  // topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  // Verifica a distancia de cada objeto ao topo
+  // em relecao o scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
-    this.scrollAnimation();
-    window.addEventListener("scroll", this.scrollAnimation);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
 
-    return this
+    return this;
+  }
+
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
